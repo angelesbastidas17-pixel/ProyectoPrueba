00e496961f88b00682834f0534321f28c409a654
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-17 14:16:55 -0500

    index for

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 var contador = prompt("Ingresa un numero <10: ");
 
-while(contador <=10){
-    console.log("va por: "+contador);
-    contador++;
+for(var i=0;i<contador;i++){
+    console.log("va en: " + contador);
 }

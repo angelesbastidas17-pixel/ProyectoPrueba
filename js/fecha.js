610c2f65178e8a9954f79bbcb46d42fbddfef770
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-17 18:30:41 -0500

    .

--- js/fecha.js
+++ js/fecha.js
@@ -1,2 +1,8 @@
-var fecha = new Date();
-alert("La fecha de hoy es: "+fecha);
+var fecha = new Date(2003,9,16);
+//var fecha = new Date("10/01/2026");
+
+var dia = fecha.getDate();
+var mes = fecha.getMonth();
+var año = fecha.getFullYear();
+
+alert("dia: "+dia+" mes "+(mes+1)+" año "+año);

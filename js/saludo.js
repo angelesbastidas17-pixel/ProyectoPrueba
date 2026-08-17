30acc0a8748cba62854124839c6cb67f53f89c37
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-17 11:59:13 -0500

    .

--- js/saludo.js
+++ js/saludo.js
@@ -4,5 +4,8 @@ var presenteONo = true;
 var apellido = "Bastidas";
 var estatura = 1.56;
 
+console.log("Nombre: "+ nombre + " " + typeof nombre);
+console.log("Edad: "+ edad);
+console.log("Presente: "+ presenteONo);
 
 //alert("Hola mundo con Javascript " + nombre);

aac5be376856883d6412b6b900e384b50136fb9b
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-17 13:53:02 -0500

    index con el switch

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var color = prompt ("Ingrese un color");
+
+switch(color){
+
+    case "rojo":
+        console.log("color rojo sojo");
+        break;
+    case "azul":
+        console.log("azuliño");
+        break;
+    default:
+        console.log("el color ingresado no es valido");
+        break;
+}

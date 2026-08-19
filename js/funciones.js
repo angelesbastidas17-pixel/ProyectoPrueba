fdca443bb156d6d278eeb0aa5ef44c9238c581a4
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-19 09:02:26 -0500

    funciones

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+function sumaSi(num1,num2){
+    if(num1>num2){
+        return num1*2+num2;
+    }else{
+        return num1+num2;
+    }
+}
+
+console.log(sumaSi(9,3));
+console.log(sumaSi(5,8));

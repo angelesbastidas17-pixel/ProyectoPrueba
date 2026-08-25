c15e91100d15a1e5f99c394ba9b5939ea393d6ac
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-25 12:15:28 -0500

    .

---
+++ js/ensayo.js
@@ -0,0 +1,15 @@
+let nombres = ["pikachu","Charmi","Squirrel"];
+let nombreCompleto = "Ana Sofia Rivadeneira de Hernandez San Miguel"
+
+/*
+Extrae una sección
+*/
+console.log(nombres.slice(-2))
+console.log(nombreCompleto.slice(-6))
+
+/*
+Extrae un solo valor, por eso
+console.log(nombres.at(nombres.length)) // undefined
+*/
+console.log(nombres.at(2))
+console.log(nombreCompleto.at(-6))

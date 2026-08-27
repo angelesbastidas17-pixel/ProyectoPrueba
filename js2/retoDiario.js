83681cfef10c2fb9215bba5d90513f8bdfe6e9f6
angelesbastidas17-pixel <angelesbastidas17@gmail.com>
2026-08-26 20:43:43 -0500

    .

--- js2/retoDiario.js
+++ js2/retoDiario.js
@@ -2,30 +2,46 @@ let sol = "akhdaedekdedesds"
 let solV = sol.split("").reverse().join("")
 console.log(solV)
 
-let hola = "holas y los pandas";
-console.log(hola.split("s").join(""))
-let holaN = hola.slice(2,6)
-console.log(holaN)
+// let hola = "holas y los pandas";
+// console.log(hola.split("s").join(""))
+// let holaN = hola.slice(2,6)
+// let holaLong = hola.split("")
+// holaLong.slice(2,6)
+// console.log(holaN)
+// console.log(holaLong)
 
 let parte1 = "hola "
 let parte2 = "mundo"
 console.log(parte1.concat(parte2))
 
+let hola = "holas y los pandas";
+let holaLong = hola.split("")
+
+while(holaLong.includes("s")){
+    holaLong.pop()
+}
+console.log(holaLong)
+
 let s = "(f(b(dc)e)a)"
 
-function cambio (s){
-    for(let i = 0; i<s.length;i++){
-        if(s.at(i)==")"){
-            let scopy = s.slice(0,i);
-            let indice1 = scopy.lastIndexOf("C")
-            let mini = scopy.slice(indice1+1,i)
-            let limpio = mini.split("").reverse().join("")
-            let parteA = s.slice(0,indice1)
-            let parteB = s.slice(i+1,s.length)
-            var snueva = (parteA.concat(limpio).concat(parteB));
-        }
-    }
-    return snueva
+while(s.includes("(") || s.includes(")")){
+    
 }
 
-console.log(cambio(s));
+// function cambio (s){
+//     let scopia = s.slice(0,s.length+1);
+//     for(let i = 0; i<scopia.length;i++){
+//         if(s.at(i)==")"){
+//             let scopy = s.slice(0,i);
+//             let indice1 = scopy.lastIndexOf("C")
+//             let mini = s.slice(indice1+1,i)
+//             let limpio = mini.split("").reverse().join("")
+//             let parteA = s.slice(0,indice1)
+//             let parteB = s.slice(i+1,s.length)
+//             var s = (parteA.concat(limpio).concat(parteB));
+//         }
+//     }
+//     return s
+// }
+
+// console.log(cambio(s));
